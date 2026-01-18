4fa33b20b6ef121d815bdd8cc13bf7ed1f25af98
unknown <edudandreamt@gmail.com>
2026-01-18 20:46:12 -0300

    Criação DokerFile2

--- Back/HumanCRM-Api/Program.cs
+++ Back/HumanCRM-Api/Program.cs
@@ -6,20 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-    builder.Services.AddCors(opt =>
+builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
         policy.AllowAnyHeader()
               .AllowAnyMethod()
-              .WithOrigins("http://192.168.1.100:4200", "http://localhost:4200");
+              .AllowAnyOrigin(); // 👈 produção (Railway)
     });
 });
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -27,20 +24,24 @@ var app = builder.Build();
 
 app.UseCors("CorsPolicy");
 
-// Configure the HTTP request pipeline.
+// ⚠️ IMPORTANTE: Railway já é HTTPS
+// app.UseHttpsRedirection(); ❌ REMOVER
+
+// 👇 ESSENCIAL PARA O ANGULAR
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
-app.MapFallbackToFile("index.html");
-
 app.MapControllers();
+
+// 👇 SEMPRE POR ÚLTIMO
+app.MapFallbackToFile("index.html");
 
 app.Run();
