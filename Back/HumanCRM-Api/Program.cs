113ff4e429f474f9ba7cf59920fcde6d2f1a61d5
unknown <edudandreamt@gmail.com>
2026-01-19 21:15:54 -0300

    Ajuste API

--- Back/HumanCRM-Api/Program.cs
+++ Back/HumanCRM-Api/Program.cs
@@ -23,6 +23,12 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    db.Database.Migrate();
+}
+
 // CORS
 app.UseCors("CorsPolicy");
 
