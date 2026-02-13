6f215b406c3cd3d7dfbcf7529e3976e480df6dee
unknown <edudandreamt@gmail.com>
2026-02-13 08:56:06 -0300

    Atualização do Banco de dados

--- Back/HumanCRM-Api/Program.cs
+++ Back/HumanCRM-Api/Program.cs
@@ -24,24 +24,6 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
-
-    for (var i = 1; i <= 5; i++)
-    {
-        try
-        {
-            db.Database.Migrate();
-            break;
-        }
-        catch
-        {
-            if (i == 5) throw;
-            Thread.Sleep(3000);
-        }
-    }
-}
 
 // CORS
 app.UseCors("CorsPolicy");
@@ -49,7 +31,6 @@ app.UseCors("CorsPolicy");
 // Static files
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
