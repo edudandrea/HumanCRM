e37b41211c9618cb574d486de5e6a4847729f99f
unknown <edudandreamt@gmail.com>
2026-02-16 11:18:51 -0300

    Correçao Bugs

--- Back/HumanCRM-Api/Models/Clientes.cs
+++ Back/HumanCRM-Api/Models/Clientes.cs
@@ -45,7 +45,7 @@ namespace HumanCRM_Api.Models
         public DateTime? DataFuncacao { get; set; }  
         public DateTime? DataContato { get; set; }    
         public DateTime? DataNascimento { get; set; } 
-        public ICollection<ProspeccaoCliente> Prospeccoes { get; set; } = new List<ProspeccaoCliente>();
-        public ICollection<ContratoCliente> Contratos { get; set; }
+        public ICollection<ProspeccaoCliente>? Prospeccoes { get; set; } = new List<ProspeccaoCliente>();
+        public ICollection<ContratoCliente>? Contratos { get; set; }
     }
 }
